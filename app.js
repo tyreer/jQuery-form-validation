48c94e8d03492639f8d4883484a2831cb26233e8
Robert Tyree <tyreer@gmail.com>
2017-04-22 21:12:25 +0100

    Added initial form verification test

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+var $name = $("#name");
+
+//Hide hints
+$("form span").hide();
+
+function isNameValid() {
+  return $name.val().length >= 1;
+}
+
+function nameEvent(){
+    //Find out if name is valid
+    if(isNameValid()) {
+      //Hide hint if valid
+      $name.next().hide();
+    } else {
+      //else show hint
+      $name.next().show();
+    }
+}
+
+//When event happens on name input
+$name.focus(nameEvent).keyup(nameEvent);
+
 $('button').on('click', function() {
-  var $companyName = $('#company-name');
-  console.log($companyName[0].value);
+  console.log($name.val());
 })
