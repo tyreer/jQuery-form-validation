128a896c2ba48f7e0c6c5232c0ffedfc3828dbe4
Robert Tyree <tyreer@gmail.com>
2017-04-22 21:39:12 +0100

    Built out basic multi-input form validation logic. Styled form.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var $name = $("#name");
+const $name = $("#name");
+const $password = $('#password');
+const $passwordConfirm = $('#password-confirm');
+
 
 //Hide hints
 $("form span").hide();
 
-function isNameValid() {
+function isFreeTextValid() {
   return $name.val().length >= 1;
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $passwordConfirm.val();
+}
+
+function canSubmit() {
+  return isFreeTextValid() && arePasswordsMatching();
 }
 
 function nameEvent(){
     //Find out if name is valid
-    if(isNameValid()) {
+    if(isFreeTextValid()) {
       //Hide hint if valid
       $name.next().hide();
     } else {
@@ -18,9 +29,36 @@ function nameEvent(){
     }
 }
 
+function passwordEvent(){
+    //Find out if password is valid
+    if(isFreeTextValid()) {
+      //Hide hint if valid
+      $password.next().hide();
+    } else {
+      //else show hint
+      $password.next().show();
+    }
+}
+
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if(arePasswordsMatching()) {
+    //Hide hint if match
+    $confirmPassword.next().hide();
+  } else {
+    //else show hint
+    $confirmPassword.next().show();
+  }
+}
+
+
 //When event happens on name input
 $name.focus(nameEvent).keyup(nameEvent);
 
 $('button').on('click', function() {
-  console.log($name.val());
+  if (canSubmit()){
+    alert("YAS");
+  } else {
+    alert("NAH BRAH");
+  }
 })
