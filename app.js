d0bb3715cdef12fee86c581ffb61715b922a8d47
Robert Tyree <tyreer@gmail.com>
2017-04-23 08:48:12 +0100

    Added basic error handling for name input and revised success event on button click.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const $companyName = $("#company-name");
-const $name = $("#name");
-const $email = $("#email");
-const $phone = $("#phone");
-const $password = $("#password");
-const $passwordConfirm = $("#password-confirm");
+const $companyName = $('#company-name');
+const $name = $('#name');
+const $email = $('#email');
+const $phone = $('#phone');
+const $password = $('#password');
+const $passwordConfirm = $('#password-confirm');
+const $button = $('button');
+// let failureArray = [];
 
 
 //Hide hints
@@ -14,17 +16,22 @@ function isCompanyNameValid() {
 }
 
 function isNameValid() {
-  return $name.val().length >= 1;
+  if ($name.val().length >= 1) {
+    $name.next().hide();
+    return true;
+  } else {
+    $name.next().show();
+  }
 }
 
 function isEmailValid() {
-  return($email.is(":valid"))
+  return $email.is(":valid")
 }
 
 function isPhoneValid() {
   const phoneDigits = $phone.val().replace(/\D/g,'');
   // Incomplete validation. Matches most UK numbers though
-  return ((phoneDigits.length === 10 || phoneDigits.length === 11));
+  return (phoneDigits.length === 10 || phoneDigits.length === 11);
 }
 
 function arePasswordsMatching() {
@@ -35,16 +42,16 @@ function canSubmit() {
   return (isCompanyNameValid() && isNameValid() &&  arePasswordsMatching() && isPhoneValid() && isEmailValid());
 }
 
-// function nameEvent(){
-//     //Find out if name is valid
-//     if(isFreeTextValid()) {
-//       //Hide hint if valid
-//       $name.next().hide();
-//     } else {
-//       //else show hint
-//       $name.next().show();
-//     }
-// }
+function nameEvent(){
+    //Find out if name is valid
+    if(isNameValid()) {
+      //Hide hint if valid
+      $name.next().hide();
+    } else {
+      //else show hint
+      $name.next().show();
+    }
+}
 //
 // function passwordEvent(){
 //     //Find out if password is valid
@@ -72,10 +79,11 @@ function canSubmit() {
 //When event happens on name input
 // $name.focus(nameEvent).keyup(nameEvent);
 
-$('button').on('click', function() {
+$button.on('click', function() {
   if (canSubmit()){
-    alert("YAS");
-  } else {
-    alert("NAH BRAH");
+    $button.replaceWith('<h3>Successfully submitted. Thank you!</h3>');
   }
+  // else {
+  //   alert("NAH BRAH");
+  // }
 })
