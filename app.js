119d06fcbedbac960f458424854783c1958cc4eb
Robert Tyree <tyreer@gmail.com>
2017-04-23 07:50:00 +0100

    Added basic input validation for all fields

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const $name = $("#name");
-const $password = $('#password');
-const $passwordConfirm = $('#password-confirm');
+const $email = $("#email");
+const $phone = $("#phone");
+const $password = $("#password");
+const $passwordConfirm = $("#password-confirm");
 
 
 //Hide hints
@@ -14,46 +16,56 @@ function arePasswordsMatching() {
   return $password.val() === $passwordConfirm.val();
 }
 
+function isEmailValid() {
+  return($email.is(":valid"))
+}
+
+function isPhoneValid() {
+  const phoneDigits = $phone.val().replace(/\D/g,'');
+  // Incomplete validation. Matches most UK numbers though
+  return ((phoneDigits.length === 10 || phoneDigits.length === 11));
+}
+
 function canSubmit() {
-  return isFreeTextValid() && arePasswordsMatching();
+  return (isFreeTextValid() && arePasswordsMatching() &&isPhoneValid() && isEmailValid());
 }
 
-function nameEvent(){
-    //Find out if name is valid
-    if(isFreeTextValid()) {
-      //Hide hint if valid
-      $name.next().hide();
-    } else {
-      //else show hint
-      $name.next().show();
-    }
-}
+// function nameEvent(){
+//     //Find out if name is valid
+//     if(isFreeTextValid()) {
+//       //Hide hint if valid
+//       $name.next().hide();
+//     } else {
+//       //else show hint
+//       $name.next().show();
+//     }
+// }
+//
+// function passwordEvent(){
+//     //Find out if password is valid
+//     if(isFreeTextValid()) {
+//       //Hide hint if valid
+//       $password.next().hide();
+//     } else {
+//       //else show hint
+//       $password.next().show();
+//     }
+// }
 
-function passwordEvent(){
-    //Find out if password is valid
-    if(isFreeTextValid()) {
-      //Hide hint if valid
-      $password.next().hide();
-    } else {
-      //else show hint
-      $password.next().show();
-    }
-}
-
-function confirmPasswordEvent() {
-  //Find out if password and confirmation match
-  if(arePasswordsMatching()) {
-    //Hide hint if match
-    $confirmPassword.next().hide();
-  } else {
-    //else show hint
-    $confirmPassword.next().show();
-  }
-}
+// function confirmPasswordEvent() {
+//   //Find out if password and confirmation match
+//   if(arePasswordsMatching()) {
+//     //Hide hint if match
+//     $confirmPassword.next().hide();
+//   } else {
+//     //else show hint
+//     $confirmPassword.next().show();
+//   }
+// }
 
 
 //When event happens on name input
-$name.focus(nameEvent).keyup(nameEvent);
+// $name.focus(nameEvent).keyup(nameEvent);
 
 $('button').on('click', function() {
   if (canSubmit()){
